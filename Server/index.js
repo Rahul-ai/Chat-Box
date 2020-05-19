3383625c14981d827f4322aef64492993972a4fb
Rahul <>
2020-05-19 13:25:05 +0530

    Day 1

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+const express =require('express');
+const socketio=require('socket.io');
+const http =require('http');
+
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+const app=express();
+const server =http.createServer(app);
+const io=socketio(server);
+io.on('connection',(socket) => {
+	console.log('connected');
+	socket.on('disconnect',() =>{
+		console.log('disconnect');
+	})
+});
+app.use(router);
+server.listen(PORT,() =>console.log(`Server has start on port ${PORT}`)); 
