28d8ac29660bc9a706df58d1c02015443aba0de4
Rahul <55375986+Rahul-ai@users.noreply.github.com>
2020-05-19 22:36:36 +0530

    message sending

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,30 @@
 const express =require('express');
 const socketio=require('socket.io');
 const http =require('http');
-
+const {addUser ,removeUser, getUser, getUserInRoom} =require('./user.js');
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
 const app=express();
 const server =http.createServer(app);
 const io=socketio(server);
+app.use(router);
 io.on('connection',(socket) => {
-	console.log('connected');
+	socket.on('join',({name, room},callback) =>{
+	const {error ,user}=addUser({id: socket.id,name,room});
+      if(error) return callback(error);
+    
+     socket.emit('message',{user:'admin', text:`${user.name}, welcome to the room ${user.room}.`});
+     socket.broadcast.to(user.room).emit('message',{user: 'admin', textt: `${user.name}, has joined`});
+	socket.join(user.room);
+	callback();
+	}); 
+	socket.on('sendMessage',(message, callback) =>{
+		const user=getUser(socket.id);
+		io.to(user.room).emit('message',{user: user.name, text:message});
+		callback();
+	});
 	socket.on('disconnect',() =>{
 		console.log('disconnect');
 	})
 });
-app.use(router);
 server.listen(PORT,() =>console.log(`Server has start on port ${PORT}`)); 
